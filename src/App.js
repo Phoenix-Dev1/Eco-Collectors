be5f58f07a006e9610eb123d6944798429c40dc1
Phoenix-Dev2 <116957217+Phoenix-Dev2@users.noreply.github.com>
2023-05-12 11:26:54 +0300

    installing tailwind
    config and testing

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import React from 'react';
 import Header from './components/header/Header';
 import Map from './components/map/Map';
 
-import './App.css';
+import './index.css';
 
 function App() {
   console.log('In App');
   return (
     <>
-      <Header heading="Test 0.1.0 - Bar Kaziro" />
+      <Header heading="Test 0.1.0 - Bar Kaziro & Liran Barzilai" />
       <Map />
     </>
   );
