516de11e82d74296d1ddb5f155a98e230629e981
Liranbar98 <131974352+Liranbar98@users.noreply.github.com>
2023-05-12 13:30:20 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+<<<<<<< Updated upstream
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
+=======
+  content: [],
+>>>>>>> Stashed changes
   theme: {
     extend: {},
   },
   plugins: [],
+<<<<<<< Updated upstream
 };
+=======
+}
+
+>>>>>>> Stashed changes
