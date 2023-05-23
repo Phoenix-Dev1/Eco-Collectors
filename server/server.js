2aef1266be103432e8b741a7dfd1b001d173006d
Phoenix-Dev2 <116957217+Phoenix-Dev2@users.noreply.github.com>
2023-05-23 14:37:13 +0300

    Rollback

--- server/server.js
+++ server/server.js
@@ -4,23 +4,15 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const app = express();
 const authRoutes = require('./routes/auth.js');
-const usersRoutes = require('./routes/users.js');
 const requestsRoutes = require('./routes/requests.js');
 const port = process.env.PORT || 8800;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-/* Testing - data try
-app.get('/api', (req, res) => {
-  res.json({ users: ['userOne', 'userTwo', 'userThree'] });
-});
-*/
-
 // Middlewares
 app.use(express.json());
 app.use(cookieParser());
 app.use('/api/auth', authRoutes);
-app.use('/api/users', usersRoutes);
 app.use('/api/requests', requestsRoutes);
 
 /*
