96881f1bbfc2cb48d0721d741749fa584bbe05d6
Phoenix-Dev2 <116957217+Phoenix-Dev2@users.noreply.github.com>
2023-06-21 10:27:00 +0300

    font awesome libraries

--- client/src/pages/Home/home.js
+++ client/src/pages/Home/home.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { faAngleUp } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Home = () => {
   return (
@@ -176,6 +178,12 @@ const Home = () => {
             wipes, waste that is not packaging, and any waste that cannot be
             sorted in the other bins.
           </p>
+          <a
+            href="#"
+            className="fixed w-12 h-12 bg-blue-700 bottom-12 right-8 flex items-center justify-center text-black text-lg rounded-full hover:text-white"
+          >
+            <FontAwesomeIcon icon={faAngleUp} />
+          </a>
         </div>
       </main>
     </div>
