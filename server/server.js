935c3b8ce55f42935403b00b56d4a3f054d878e5
Liranbar98 <131974352+Liranbar98@users.noreply.github.com>
2023-08-03 16:37:19 +0300

    Pages and handler created no functionality yet.
    Created a new folder inside Users folder that contains all the welcome pages and the handler itself. updated server.js and App.js with the new changes.

--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@ const recyclersManagers = require('./routes/recyclersManagerRegister.js');
 const user = require('./routes/user.js');
 const dashboardUser = require('./routes/dashboardUser.js');
 const dashboardRecycler = require('./routes/dashboardRecycler.js');
+const welcomeUser = require('./routes/welcomeUser.js')
 const port = process.env.PORT || 8800;
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -32,6 +33,7 @@ app.use('/api/recyclersManagers', recyclersManagers);
 app.use('/api/user', user);
 app.use('/api/dashboardUser', dashboardUser);
 app.use('/api/dashboardRecycler', dashboardRecycler);
+app.use('/api/user/welcome', welcomeUser);
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
