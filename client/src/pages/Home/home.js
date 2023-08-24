3fe3069c93b26fc34ee03251a85fe11f0c453e08
Liranbar98 <131974352+Liranbar98@users.noreply.github.com>
2023-08-24 18:52:03 +0300

    Final styles for home page.
    Added home.css, QuizCard.js and quizData.js.
    updated home page, now complete!

--- client/src/pages/Home/home.js
+++ client/src/pages/Home/home.js
@@ -1,13 +1,18 @@
+import './home.css';
 import React from 'react';
+import QuizCard from './QuizCard';
+import { quizData } from './quizData';
 import { faAngleUp } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Home = () => {
+  const randomIndex = Math.floor(Math.random() * quizData.length);
+  const randomQuiz = quizData[randomIndex];
   return (
     <div className="font-sans bg-gray-800 dark:bg-gray-800 text-white">
       <main className="container mx-auto py-8">
         <div className="max-w-3xl mx-auto">
-          <h1 className="text-5xl font-semibold mb-12 text-center">
+          <h1 className="text-5xl font-semibold mb-12 text-center unique-text">
             Welcome to Eco Collectors! ♻️
           </h1>
           <p className="text-lg mb-12 text-center">
@@ -16,58 +21,67 @@ const Home = () => {
             Together, we can create a greener Israel for the future.
           </p>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-            {/* Bin Cards */}
-            <div className="bg-blue-500 p-6 rounded-md shadow-md">
+            {/* Blue Bin */}
+            <div className="bin-card bg-blue-500 p-6 rounded-md shadow-md transition duration-300 transform hover:scale-105">
               <h2 className="text-xl font-semibold mb-2">Blue Bin</h2>
-              <p className="text-gray-200">
+              <p className="text-gray-300">
                 Newspapers, magazines, cardboard, paper packaging, and more.
               </p>
             </div>
-            <div className="bg-orange-900 p-6 rounded-md shadow-md">
+            {/* Brown Bin */}
+            <div className="bin-card bg-orange-900 p-6 rounded-md shadow-md transition duration-300 transform hover:scale-105">
               <h2 className="text-xl font-semibold mb-2">Brown Bin</h2>
-              <p className="text-gray-200">
+              <p className="text-gray-300">
                 Fruits, vegetables, food scraps, and more.
               </p>
             </div>
-            <div className="bg-orange-500 p-6 rounded-md shadow-md">
+            {/* Orange Bin */}
+            <div className="bin-card bg-orange-500 p-6 rounded-md shadow-md transition duration-300 transform hover:scale-105">
               <h2 className="text-xl font-semibold mb-2">Orange Bin</h2>
-              <p className="text-gray-200">
+              <p className="text-gray-300">
                 Plastic packaging, metal packaging, beverage cartons, and more.
               </p>
             </div>
-            <div className="bg-purple-500 p-6 rounded-md shadow-md">
+            {/* Purple Bin */}
+            <div className="bin-card bg-purple-500 p-6 rounded-md shadow-md transition duration-300 transform hover:scale-105">
               <h2 className="text-xl font-semibold mb-2">Purple Bin</h2>
-              <p className="text-gray-200">
+              <p className="text-gray-300">
                 Glass bottles, jars, and glass packaging.
               </p>
             </div>
-            <div className="bg-gray-500 p-6 rounded-md shadow-md">
+            {/* Gray Bin */}
+            <div className="bin-card bg-gray-500 p-6 rounded-md shadow-md transition duration-300 transform hover:scale-105">
               <h2 className="text-xl font-semibold mb-2">Gray Bin</h2>
-              <p className="text-gray-200">
+              <p className="text-gray-300">
                 Metallic waste. Recycled by melting and casting into new
                 products.
               </p>
             </div>
-            <div className="bg-yellow-500 p-6 rounded-md shadow-md">
+            {/* Cartoner */}
+            <div className="bin-card bg-yellow-500 p-6 rounded-md shadow-md transition duration-300 transform hover:scale-105">
               <h2 className="text-xl font-semibold mb-2">Cartoner</h2>
-              <p className="text-gray-200">
+              <p className="text-gray-300">
                 Thick cardboard packages. Flatten before disposal.
               </p>
             </div>
-            <div className="bg-red-500 p-6 rounded-md shadow-md">
+            {/* E-Waste */}
+            <div className="bin-card bg-red-500 p-6 rounded-md shadow-md transition duration-300 transform hover:scale-105">
               <h2 className="text-xl font-semibold mb-2">E-Waste</h2>
-              <p className="text-gray-200">
+              <p className="text-gray-300">
                 Dispose old electrical appliances, batteries, and more at
                 collection facilities.
               </p>
             </div>
-            <div className="bg-green-700 p-6 rounded-md shadow-md">
+            {/* Green Bin */}
+            <div className="bin-card bg-green-700 p-6 rounded-md shadow-md transition duration-300 transform hover:scale-105">
               <h2 className="text-xl font-semibold mb-2">Green Bin</h2>
-              <p className="text-gray-200">
+              <p className="text-gray-300">
                 For waste that can't be sorted elsewhere. Some materials may be
                 sorted and recycled.
               </p>
             </div>
+            {/* Random Quiz Card */}
+            <QuizCard quiz={randomQuiz} />
           </div>
           <div className="flex justify-center mt-16">
             <a
